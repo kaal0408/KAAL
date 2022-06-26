b36feb5b90ed9ad7617215df414e1e7bfb2544ce
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-06-26 18:54:37 +0530

    Update omk.py

--- omk.py
+++ omk.py
@@ -2,5 +2,5 @@ from os import system
 from logging import DEBUG, INFO, basicConfig, getLogger, warning
 basicConfig(format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s", level=INFO)
 LOGS = getLogger("Helper")
-system("git clone https://github.com/kaal0408/KAAL && cd KAAL && python3 System")
+system("git clone https://github.com/kaal0408/KAAL && cd KAAL && python3 -m System")
 print("lul got error")
